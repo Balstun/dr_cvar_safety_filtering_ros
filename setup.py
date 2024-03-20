9303703505f93f656ecc699c8bdc5f0dccc7f660
Sai Bommisetty <EWizard512@gmail.com>
2024-03-20 18:04:36 -0500

    Add setup.py file to install packages within the module

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dr_cvar_safety_filtering_ros',
+    packags=find_packages(),
+    author='Sleiman Safaoui',
+    author_email='sleiman_safaoui@utdallas.edu',
+    description='Data-Driven Distributionally-Robust CVaR-based Motion Planning Module',
+    url='https://github.com/Balstun/dr_cvar_safety_filtering_ros'
+)
