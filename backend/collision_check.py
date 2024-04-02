f598d4a6ca7f10104584b96d3d77547e7c3c88e7
Sai Bommisetty <EWizard512@gmail.com>
2024-04-02 04:02:31 -0500

    Add feature to calculate collision distance by axial distance metric

---
+++ backend/collision_check.py
@@ -0,0 +1,16 @@
+from backend.dynamic_vehicles import RectangularKinematicBicycle
+
+def distance_to_collsion(ego_veh: RectangularKinematicBicycle, obst_veh: RectangularKinematicBicycle):
+    # Collect vehicle geometry data
+    ego_center = ego_veh.center
+    ego_width = ego_veh.width
+    ego_height = ego_veh.height
+    obst_center = obst_veh.center
+    obst_width = obst_veh.width
+    obst_height = obst_veh.height
+
+    # Calculate axial distance to collision
+    distance = max(abs(ego_center[0] - obst_center[0]) - (ego_width + obst_width)/2,
+                   abs(ego_center[1] - obst_center[1]) - (ego_height + obst_height)/2)
+
+    return distance
